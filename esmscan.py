ebc1e376c124f170b99872a35d76cdac36560877
Katherine <katherine@katherines-air.dyn.nyp.org>
2023-10-30 14:35:34 -0400

    esmscan

--- esmscan.py
+++ esmscan.py
@@ -18,68 +18,103 @@ from typing import List, Tuple
 import numpy as np
 from matplotlib import pylab
 
+
 # xw
-def generate_all_mutations(sequence,output_file):
+def generate_all_mutations(sequence, output_file):
     aas = 'ACDEFGHIKLMNPQRSTVWY'
-    with open(output_file,'w') as f:
+    with open(output_file, 'w') as f:
         f.write('mutant\n')
         for i in range(len(sequence)):
             for aa in aas:
-                f.write(sequence[i]+str(i+1)+aa+'\n')
+                f.write(sequence[i] + str(i + 1) + aa + '\n')
     f.close()
 
-def generate_user_mutations(mutation,output_file):
-    with open(output_file,'w') as f:
+
+def generate_user_mutations(mutation, output_file):
+    with open(output_file, 'w') as f:
         f.write('mutant\n')
         l = mutation.split(",")
         for i in l:
-          f.write(i+'\n')
+            f.write(i + '\n')
     f.close()
 
+
 # xw
-def plot_esm_scan(seq,output_prefix):
-    # read output in a list format
-    df=pd.read_csv(str(output_prefix)+'-res-in-list.csv')
-    
-    # convert to a matrix, each row is a wild type aa/position in the sequence, each column is a mutation
-    d = np.array(df[ df.columns[1] ]).reshape(len(seq),20)
-    
-    # convert it to a data frame
-    df2 = pd.DataFrame(d, columns = [aa for aa in 'ACDEFGHIKLMNPQRSTVWY'], index = [seq[x]+str(x+1) for x in range(len(seq))] )
-    df2.to_csv(str(args.output_prefix)+'-res-in-matrix.csv')
-    
-    # plot matrix as heatmap
-    pylab.rcParams['pdf.fonttype']=42
-    pylab.rcParams['font.size']=12
-    pylab.rcParams['figure.figsize'] = [8, len(seq)/3]
+def plot_clinvar(df, output_prefix):
+    pathogenic_scores = np.load('./ESM-Scan/pathogenic_scores.npy')
+    benign_scores = np.load('./ESM-Scan/benign_scores.npy')
+    pylab.rcParams['pdf.fonttype'] = 42
+    pylab.rcParams['font.size'] = 12
+    pylab.rcParams['figure.figsize'] = [10, 6]
 
     fig, ax = pylab.subplots()
-    im = ax.imshow(d,vmin=np.min(d),vmax=np.max(d),cmap='bwr',aspect='equal')
+
+    # Create histograms with density=True to normalize the data
+    ax.hist(pathogenic_scores, bins=300, density=True, color='red', alpha=0.5, label='Pathogenic')
+    ax.hist(benign_scores, bins=300, density=True, color='blue', alpha=0.5, label='Benign')
+
+    values_to_plot = df['/content/esm1b_t33_650M_UR50S.pt']
+    labels = df['mutant']
+    for label, value in zip(labels, values_to_plot):
+        ax.axvline(x=value, color='red', linestyle='--', alpha=0.5)  # Customize the line style and transparency
+        ax.text(value, 0.01, label, rotation=90, verticalalignment='bottom', horizontalalignment='right')
+
+    # Add labels and legend
+    ax.set_xlabel('Scores')
+    ax.set_ylabel('Density')
+    ax.legend(loc='upper right')
+
+    # Show the plot
+    ax.set_title('Pathogenic vs. Benign Score Density Plot and Histogram')
+    pylab.close()
+    fig.savefig(str(output_prefix) + '-graph.pdf')
+
+
+def plot_esm_scan(seq, output_prefix):
+    # read output in a list format
+    df = pd.read_csv(str(output_prefix) + '-res-in-list.csv')
+
+    # convert to a matrix, each row is a wild type aa/position in the sequence, each column is a mutation
+    d = np.array(df[df.columns[1]]).reshape(len(seq), 20)
+
+    # convert it to a data frame
+    df2 = pd.DataFrame(d, columns=[aa for aa in 'ACDEFGHIKLMNPQRSTVWY'],
+                       index=[seq[x] + str(x + 1) for x in range(len(seq))])
+    df2.to_csv(str(args.output_prefix) + '-res-in-matrix.csv')
+
+    # plot matrix as heatmap
+    pylab.rcParams['pdf.fonttype'] = 42
+    pylab.rcParams['font.size'] = 12
+    pylab.rcParams['figure.figsize'] = [8, len(seq) / 3]
+
+    fig, ax = pylab.subplots()
+    im = ax.imshow(d, vmin=np.min(d), vmax=np.max(d), cmap='bwr', aspect='equal')
 
     ax.set_ylabel('Wild type')
-    yticks=list(range(len(seq)))
+    yticks = list(range(len(seq)))
     ax.set_yticks(yticks)
-    ax.set_yticklabels( seq[x]+str(x+1) for x in range(len(seq)))
+    ax.set_yticklabels(seq[x] + str(x + 1) for x in range(len(seq)))
 
     ax.xaxis.tick_top()
-    ax.xaxis.set_label_position('top') 
+    ax.xaxis.set_label_position('top')
     ax.set_xlabel('Mutant')
-    xticks=list(range(20))
+    xticks = list(range(20))
     ax.set_xticks(xticks)
     ax.set_xticklabels(aa for aa in 'ACDEFGHIKLMNPQRSTVWY')
 
     # colorbar
-    fig.colorbar(im,orientation="horizontal")
+    fig.colorbar(im, orientation="horizontal")
 
-    fig.savefig(str(output_prefix)+'-matrix.pdf')
-    
+    fig.savefig(str(output_prefix) + '-matrix.pdf')
+
     # boxplot
     pylab.close()
-    pylab.rcParams['pdf.fonttype']=42
-    pylab.rcParams['font.size']=12
-    pylab.rcParams['figure.figsize'] = [8, len(seq)/3]
-    boxplot = df2[::-1].T.boxplot(grid=False,vert=False) 
-    pylab.savefig(str(output_prefix)+'-boxplot.pdf')
+    pylab.rcParams['pdf.fonttype'] = 42
+    pylab.rcParams['font.size'] = 12
+    pylab.rcParams['figure.figsize'] = [8, len(seq) / 3]
+    boxplot = df2[::-1].T.boxplot(grid=False, vert=False)
+    pylab.savefig(str(output_prefix) + '-boxplot.pdf')
+
 
 def remove_insertions(sequence: str) -> str:
     """ Removes any insertions into the sequence. Needed to load aligned sequences in an MSA. """
@@ -94,7 +129,7 @@ def remove_insertions(sequence: str) -> str:
 
 def read_msa(filename: str, nseq: int) -> List[Tuple[str, str]]:
     """ Reads the first nseq sequences from an MSA file, automatically removes insertions.
-    
+
     The input file must be in a3m format (although we use the SeqIO fasta parser)
     for remove_insertions to work properly."""
 
@@ -147,7 +182,7 @@ def create_parser():
     parser.add_argument(
         "--offset-idx",
         type=int,
-        default=1, # xw, changed from 0 to 1
+        default=1,  # xw, changed from 0 to 1
         help="Offset of the mutation positions in `--mutation-col`"
     )
     parser.add_argument(
@@ -189,7 +224,7 @@ def compute_pppl(row, sequence, model, alphabet, offset_idx):
     assert sequence[idx] == wt, "The listed wildtype does not match the provided sequence"
 
     # modify the sequence
-    sequence = sequence[:idx] + mt + sequence[(idx + 1) :]
+    sequence = sequence[:idx] + mt + sequence[(idx + 1):]
 
     # encode the sequence
     data = [
@@ -214,21 +249,20 @@ def compute_pppl(row, sequence, model, alphabet, offset_idx):
 
 
 def main(args):
-    
-    if len(args.dms_mutation) == 0: # generate all possible mutants
-        args.dms_input = str(args.output_prefix)+'-all-mutants.txt'
-        generate_all_mutations(args.sequence,args.dms_input)    
+    if len(args.dms_mutation) == 0:  # generate all possible mutants
+        args.dms_input = str(args.output_prefix) + '-all-mutants.txt'
+        generate_all_mutations(args.sequence, args.dms_input)
     else:
-        args.dms_input = str(args.output_prefix)+'-user-mutants.txt'
-        generate_user_mutations(args.dms_mutation,args.dms_input)    
-    # Load the deep mutational scan
+        args.dms_input = str(args.output_prefix) + '-user-mutants.txt'
+        generate_user_mutations(args.dms_mutation, args.dms_input)
+        # Load the deep mutational scan
     df = pd.read_csv(args.dms_input)
 
     # inference for each model
     for model_location in args.model_location:
         model, alphabet = pretrained.load_model_and_alphabet(model_location)
         model.eval()
-                
+
         if torch.cuda.is_available() and not args.nogpu:
             model = model.cuda()
             print("Transferred model to GPU")
@@ -238,7 +272,7 @@ def main(args):
         if isinstance(model, MSATransformer):
             data = [read_msa(args.msa_path, args.msa_samples)]
             assert (
-                args.scoring_strategy == "masked-marginals"
+                    args.scoring_strategy == "masked-marginals"
             ), "MSA Transformer only supports masked marginal strategy"
 
             batch_labels, batch_strs, batch_tokens = batch_converter(data)
@@ -309,10 +343,14 @@ def main(args):
                     axis=1,
                 )
 
-    df.to_csv(str(args.output_prefix)+'-res-in-list.csv',index=False)
+    df.to_csv(str(args.output_prefix) + '-res-in-list.csv', index=False)
 
     # xw: plot
-    plot_esm_scan(args.sequence,args.output_prefix)
+    if len(args.dms_mutation) == 0:
+      plot_esm_scan(args.sequence, args.output_prefix)
+    else:
+      plot_clinvar(df, args.output_prefix)
+
 
 if __name__ == "__main__":
     parser = create_parser()
